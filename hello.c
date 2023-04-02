279d881bb41d66a1be736f1ed9a1a45e6aacafe4
zubairbinferdous <zubidev71@gmail.com>
2023-04-02 15:36:38 +0600

    ok

--- hello.c
+++ hello.c
@@ -1,8 +1,44 @@
-#include<stdio.h>
-int main(){
-    printf("hello world c programming");
-    printf("zubair bin ferdous");
-    printf("zubair bin ferdous programmer");
-    return 0 ;
+
+// kivabe print kora jai
+
+#include <stdio.h>
+int main()
+{
+    // printf("hello world \ni am  a developer \nnew line hobe");
+    // printf("hello world \t\t new line hobe");
+    // printf("\n\n");
+    // int zubair = 1000;
+    // float karim = 10.5;
+    // char habib = 's';
+    // printf("%d %0.1f %c" , zubair , karim , habib);
+
+    // inpute 30% and 40% 
+    // int numOne , numTwo ;
+    // char p ;
+    // scanf("%d%% %d%%" , &numOne,&p,&numTwo,&p );
+    // scanf("%d%% %d%%" , &numOne,&numTwo );
+    // printf("%d%% %d%%" , numOne , numTwo);
+
+    
+    return 0;
 }
 
+//    \t -> new tab -> 4 space
+//    \n -> new line
+//    \\ -> 2 ber use korle 1 ber hobe -> spiceal case
+//    %% -> 2 ber use korle 1 ber hobe -> spiceal case
+
+// variable
+
+// int = -100 , 100 , 30 ;
+// float = .4 , 5.5;
+// char = 'a' , 'A' , '@' ;
+// bool =  true or false ;
+
+// to print 
+// int -> %d 
+// float -> %f
+// char -> %c
+
+// memory (ram) -> int -> 4 byte
+// memory (ram) -> float -> 4 byte
